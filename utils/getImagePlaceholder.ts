9d73d48ef8faa30ce3efcd8da5b90383bf31e5c9
Ashfak Niloy <ashfakniloy56@gmail.com>
2023-08-29 15:28:36 +0600

    fixed blur placeholder issue with no image

--- utils/getImagePlaceholder.ts
+++ utils/getImagePlaceholder.ts
@@ -1,3 +1,61 @@
+import { getPlaiceholder } from "plaiceholder";
+import { BASE_URL } from "@/config";
+
+const getStaticBlur = async () => {
+  const staticImageUrl = `${BASE_URL}/images/placeholder.webp`;
+  const staticRes = await fetch(staticImageUrl);
+
+  const staticBuffer = await staticRes.arrayBuffer();
+
+  const { base64: staticBlurImage } = await getPlaiceholder(
+    Buffer.from(staticBuffer)
+  );
+
+  return staticBlurImage;
+};
+
+export const getImagePlaceholder = async (ogImageUrl: string) => {
+  try {
+    const imageUrl = ogImageUrl?.replace("/upload/", "/upload/w_5/");
+    const res = await fetch(imageUrl);
+
+    if (res.ok) {
+      const buffer = await res.arrayBuffer();
+
+      const { base64 } = await getPlaiceholder(Buffer.from(buffer));
+      return base64;
+    } else {
+      console.log(
+        `Failed to fetch remote image: ${res.status} ${res.statusText}`
+      );
+
+      const staticPlceholder = await getStaticBlur();
+      return staticPlceholder;
+    }
+  } catch (error) {
+    if (error instanceof Error) console.log(error.stack);
+
+    const staticPlceholder = await getStaticBlur();
+    return staticPlceholder;
+  }
+};
+
+export const getMultipleImagePlaceholder = async (imageUrls: string[]) => {
+  // making all request at once instead of awating each one - avoiding waterfall
+  const base64Promises = imageUrls.map((imageUrl) =>
+    getImagePlaceholder(imageUrl)
+  );
+
+  const base64Results = await Promise.all(base64Promises);
+
+  const photosWithBlur = imageUrls.map((imageUrl, i) => {
+    imageUrl = base64Results[i];
+    return imageUrl;
+  });
+
+  return photosWithBlur;
+};
+
 // import { getPlaiceholder } from "plaiceholder";
 // import fs from "node:fs/promises";
 
@@ -47,83 +105,3 @@
 
 //   return photosWithBlur;
 // };
-
-import { getPlaiceholder } from "plaiceholder";
-import { BASE_URL } from "@/config";
-// import fs from "node:fs/promises";
-
-export const getImagePlaceholder = async (ogImageUrl: string) => {
-  // try {
-  //   const imageUrl = ogImageUrl?.replace("/upload/", "/upload/w_5/");
-  //   const res = await fetch(imageUrl);
-
-  //   if (res.ok) {
-  //     const buffer = await res.arrayBuffer();
-
-  //     const { base64 } = await getPlaiceholder(Buffer.from(buffer));
-
-  //     return base64;
-  //   } else {
-  //     // throw new Error(`Failed to fetch image: ${res.status} ${res.statusText}`);
-  //     console.log(`Failed to fetch image: ${res.status} ${res.statusText}`);
-  //     const staticBlurImage = await getStaticBlur();
-  //     return staticBlurImage;
-  //   }
-  // } catch (error) {
-  //   if (error instanceof Error) console.log(error.stack);
-
-  //   const staticBlurImage = await getStaticBlur();
-  //   return staticBlurImage;
-  // }
-
-  const imageUrl = ogImageUrl?.replace("/upload/", "/upload/w_5/");
-  const res = await fetch(imageUrl);
-
-  // console.log("res", res.ok);
-
-  if (res.ok) {
-    const buffer = await res.arrayBuffer();
-
-    const { base64 } = await getPlaiceholder(Buffer.from(buffer));
-
-    return base64;
-  } else {
-    // throw new Error(`Failed to fetch image: ${res.status} ${res.statusText}`);
-    console.log(
-      `Failed to fetch remote image: ${res.status} ${res.statusText}`
-    );
-    // const staticBlurImage = await getStaticBlur();
-    // return staticBlurImage;
-
-    // const file = await fs.readFile(`public/images/placeholder.webp`);
-    // const { base64: staticBlurImage } = await getPlaiceholder(file);
-    // return staticBlurImage;
-
-    const staticImageUrl = `${BASE_URL}/images/placeholder.webp`;
-    const staticRes = await fetch(staticImageUrl);
-
-    const staticBuffer = await staticRes.arrayBuffer();
-
-    const { base64: staticBlurImage } = await getPlaiceholder(
-      Buffer.from(staticBuffer)
-    );
-
-    return staticBlurImage;
-  }
-};
-
-export const getMultipleImagePlaceholder = async (imageUrls: string[]) => {
-  // making all request at once instead of awating each one - avoiding waterfall
-  const base64Promises = imageUrls.map((imageUrl) =>
-    getImagePlaceholder(imageUrl)
-  );
-
-  const base64Results = await Promise.all(base64Promises);
-
-  const photosWithBlur = imageUrls.map((imageUrl, i) => {
-    imageUrl = base64Results[i];
-    return imageUrl;
-  });
-
-  return photosWithBlur;
-};
