28c70607b8ca2108242cfe8651414f2d3b8713d3
Ashfak Niloy <ashfakniloy56@gmail.com>
2023-09-27 01:54:55 +0600

    fixed protected layout

--- app/(user)/(content)/(protected)/layout.tsx
+++ app/(user)/(content)/(protected)/layout.tsx
@@ -2,6 +2,8 @@ import { redirect } from "next/navigation";
 import { getAuthSession } from "@/lib/next-auth";
 import { getUserById } from "@/db/queries/getUserById";
 
+export const dynamic = "force-dynamic";
+
 async function ProtectedLayout({ children }: { children: React.ReactNode }) {
   const session = await getAuthSession();
 
