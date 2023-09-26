75671d131d0c95c9fef02f5e7ff6090da0a7225a
Ashfak Niloy <ashfakniloy56@gmail.com>
2023-09-27 02:43:53 +0600

    upgraded nexjs to 13.5.3, updated some packages

--- app/(user)/(content)/(protected)/layout.tsx
+++ app/(user)/(content)/(protected)/layout.tsx
@@ -1,28 +1,22 @@
-// import { redirect } from "next/navigation";
-// import { getAuthSession } from "@/lib/next-auth";
-// import { getUserById } from "@/db/queries/getUserById";
+import { redirect } from "next/navigation";
+import { getAuthSession } from "@/lib/next-auth";
+import { getUserById } from "@/db/queries/getUserById";
 
-// async function ProtectedLayout({ children }: { children: React.ReactNode }) {
-//   const session = await getAuthSession();
+async function ProtectedLayout({ children }: { children: React.ReactNode }) {
+  const session = await getAuthSession();
 
-//   if (session) {
-//     const user = await getUserById({
-//       userId: session.user.id,
-//     });
+  if (session) {
+    const user = await getUserById({
+      userId: session.user.id,
+    });
 
-//     // console.log("user", user);
+    // console.log("user", user);
 
-//     if (!user) {
-//       redirect("/invalid-user");
-//     }
-//   }
+    if (!user) {
+      redirect("/invalid-user");
+    }
+  }
 
-//   return <>{children}</>;
-// }
-
-// export default ProtectedLayout;
-
-function ProtectedLayout({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
 }
 
