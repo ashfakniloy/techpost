7b1916fa7fa508da19b4682fa0525fbc2e41631e
Ashfak Niloy <ashfakniloy56@gmail.com>
2023-09-27 01:59:09 +0600

    fixed protected layout

--- app/(user)/(content)/(protected)/layout.tsx
+++ app/(user)/(content)/(protected)/layout.tsx
@@ -2,18 +2,12 @@ import { redirect } from "next/navigation";
 import { getAuthSession } from "@/lib/next-auth";
 import { getUserById } from "@/db/queries/getUserById";
 
-export const dynamic = "force-dynamic";
-
 async function ProtectedLayout({ children }: { children: React.ReactNode }) {
   const session = await getAuthSession();
 
-  // if (!session) return;
-
-  const user =
-    session &&
-    (await getUserById({
-      userId: session.user.id,
-    }));
+  const user = await getUserById({
+    userId: session?.user.id,
+  });
 
   // console.log("user", user);
 
--- db/queries/getUserById.ts
+++ db/queries/getUserById.ts
@@ -1,6 +1,6 @@
 import { prisma } from "@/lib/prisma";
 
-export async function getUserById({ userId }: { userId: string }) {
+export async function getUserById({ userId }: { userId: string | undefined }) {
   const user = await prisma.user.findUnique({
     where: {
       id: userId,
