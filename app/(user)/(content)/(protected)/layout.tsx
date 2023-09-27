ac25aa7f23134d6ca9822c55ef5a018442a1a300
Ashfak Niloy <ashfakniloy56@gmail.com>
2023-09-27 19:54:31 +0600

    downgradede nextjs and some packages for bug fix

--- app/(user)/(content)/(protected)/layout.tsx
+++ app/(user)/(content)/(protected)/layout.tsx
@@ -23,3 +23,25 @@ async function ProtectedLayout({ children }: { children: React.ReactNode }) {
 }
 
 export default ProtectedLayout;
+
+// import { redirect } from "next/navigation";
+// import { getAuthSession } from "@/lib/next-auth";
+// import { getUserById } from "@/db/queries/getUserById";
+
+// async function ProtectedLayout({ children }: { children: React.ReactNode }) {
+//   const session = await getAuthSession();
+
+//   const user = await getUserById({
+//     userId: session?.user.id,
+//   });
+
+//   // console.log("user", user);
+
+//   if (session && !user) {
+//     redirect("/invalid-user");
+//   }
+
+//   return <>{children}</>;
+// }
+
+// export default ProtectedLayout;
