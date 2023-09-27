863d338381538df41bc93ab83f58b01c27e08fb3
Ashfak Niloy <ashfakniloy56@gmail.com>
2023-09-27 17:40:20 +0600

    refactored authoptions to utils for fixing bug after deploy

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
-import { authOptions } from "@/lib/next-auth";
+import { authOptions } from "@/utils/authOptions";
+// import { authOptions } from "@/lib/next-auth";
 
 const handler = NextAuth(authOptions);
 
