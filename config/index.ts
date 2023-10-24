688a10ac66788aa1391e693736d0d78dd0224287
Ashfak Niloy <ashfakniloy56@gmail.com>
2023-10-24 13:03:02 +0600

    changed baseurl for update branch

--- config/index.ts
+++ config/index.ts
@@ -2,5 +2,6 @@ export const PER_PAGE = 10;
 
 export const BASE_URL =
   process.env.NODE_ENV !== "development"
-    ? "https://techpost.vercel.app"
+    ? // ? "https://techpost.vercel.app"
+      "https://techpost-git-update-ashfakniloy.vercel.app/"
     : "http://localhost:3000";
